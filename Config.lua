613af70ead4455d773b8aa250d04cc04625bf7c8
jessicamzfarmacia-droid <jessica.mz.farmacia@gmail.com>
2026-09-16 22:20:54 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,1 +1,12 @@
+return {
+    Size = UDim2.fromOffset(560, 350),
 
+    Colors = {
+        Background = Color3.fromRGB(22, 22, 28),
+        Sidebar = Color3.fromRGB(27, 27, 34),
+        White = Color3.fromRGB(235, 235, 240),
+        Gray = Color3.fromRGB(145, 145, 155),
+        Accent = Color3.fromRGB(90, 120, 255),
+        Off = Color3.fromRGB(55, 55, 65)
+    }
+}
